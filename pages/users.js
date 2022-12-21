6e1f563ab5fdf9f15572ff3757f8882f158071b2
Annie <anniehartmann97@gmail.com>
2022-12-21 12:54:32 +0300

    Get users

--- pages/users.js
+++ pages/users.js
@@ -1,11 +1,6 @@
 import Link from "next/link";
-import { useState } from "react";
 
-const Users = () => {
-  const [users, setUsers] = useState([
-    { id: 1, name: "Ivan" },
-    { id: 2, name: "Igor" },
-  ]);
+const Users = ({ users }) => {
   return (
     <div>
       <h1>Список пользователей</h1>
@@ -21,3 +16,12 @@ const Users = () => {
 };
 
 export default Users;
+
+export async function getStaticProps(context) {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+
+  return {
+    props: { users }, // will be passed to the page component as props
+  };
+}
--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,10 +1,18 @@
-import { useRouter } from "next/router";
-
-export default function () {
-  const router = useRouter();
+export default function ({ user }) {
   return (
     <div>
-      <h1>{router.query.id}</h1>
+      <h3>Имя пользователя - {user.name}</h3>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${context.query.id}`
+  );
+  const user = await response.json();
+
+  return {
+    props: { user }, // will be passed to the page component as props
+  };
 }
